6dad501ef791cb78eab723b1c69a5fb55920aa09
Dusan Bjelogrlic <dusanbjelogrlic0@gmail.com>
2025-01-09 16:06:33 +0100

    Rasporedjene rute

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -25,52 +25,55 @@ use App\Http\Controllers\WebServiceController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::resource('/users', UserController::class)->only(['index', 'show']);
-Route::resource('/users', UserController::class)->only(['store']);
-Route::match(['put', 'patch'],'/users/{user}', [UserController::class, 'update'])->name('users.update');
-Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
-
-Route::resource('/seasons', SeasonController::class)->only(['index', 'show']);
-Route::resource('/seasons', SeasonController::class)->only(['store']);
-Route::match(['put', 'patch'],'/seasons/{season}', [SeasonController::class, 'update']);
-Route::delete('/seasons/{season}', [SeasonController::class, 'destroy']);
-
-Route::resource('/quiz-events', QuizEventController::class)->only(['index', 'show']);
-Route::resource('/quiz-events', QuizEventController::class)->only(['store']);
-Route::match(['put', 'patch'],'/quiz-events/{quizEvent}', [QuizEventController::class, 'update'])->name('quit-events.update');
-Route::delete('/quiz-events/{quizEvent}', [QuizEventController::class, 'destroy'])->name('quit-events.destroy');
-
 Route::resource('/teams', TeamController::class)->only(['index', 'show']);
-Route::resource('/teams', TeamController::class)->only(['store']);
-Route::match(['put', 'patch'],'/teams/{team}', [TeamController::class, 'update']);
-Route::delete('/teams/{team}', [TeamController::class, 'destroy'])->name('teams.destroy');
-
-Route::get('/export-ical/{season-id}',[ExportController::class, 'exportICalendar'])->name('export-ical');
-
-Route::get('/scores/seasons/{seasonId}', [QuizEventTeamController::class, 'scoresInASeason'])->name('scores.seasons.show');
-Route::get('/scores/seasons/{seasonId}/teams/{teamId}', [QuizEventTeamController::class, 'scoresInASeasonByATeam'])->name('scores.seasons.teams.show');
-Route::post('/scores', [QuizEventTeamController::class, 'store'])->name('scores.store');
-Route::put('scores/teams/{teamId}/quiz-events/{quizEventId}', [QuizEventTeamController::class, 'update']);
-
-Route::resource('/teams.users', TeamUserController::class)->only(['index']);
+Route::resource('/seasons', SeasonController::class)->only(['index', 'show']);
+Route::resource('/quiz-events', QuizEventController::class)->only(['index', 'show']);
 
 Route::get('/search/users', [SearchController::class, 'searchUsers'])->name('search.users');
 Route::get('/search/teams', [SearchController::class, 'searchTeams'])->name('search.teams');
 Route::get('/search/quiz-events', [SearchController::class, 'searchQuizEvents'])->name('search.quiz-events');
 Route::get('/search/seasons', [SearchController::class, 'searchSeasons'])->name('search.seasons');
 
+Route::get('/export-ical/{season-id}',[ExportController::class, 'exportICalendar'])->name('export-ical');
+
+Route::get('/scores/seasons/{seasonId}', [QuizEventTeamController::class, 'scoresInASeason'])->name('scores.seasons.show');
+Route::get('/scores/seasons/{seasonId}/teams/{teamId}', [QuizEventTeamController::class, 'scoresInASeasonByATeam'])->name('scores.seasons.teams.show');
+
+Route::get('/random-question', [WebServiceController::class, 'randomQuestion'])->name('random-question');
+
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
+
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/profile', function(Request $request) {
         return auth()->user();
-    });
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-});
+    })->middleware('checkRole:admin,moderator');
 
-Route::get('/random-question', [WebServiceController::class, 'randomQuestion'])->name('random-question');
-Route::post('/register/teams', [TeamController::class, 'registerTeam'])->name('register.teams');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('/register/teams', [TeamController::class, 'registerTeam'])->name('register.teams');
+    Route::resource('/teams.users', TeamUserController::class)->only(['index']);
+
+    Route::resource('/users', UserController::class)->only(['store']);
+    Route::match(['put', 'patch'],'/users/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+
+
+    Route::resource('/seasons', SeasonController::class)->only(['store']);
+    Route::match(['put', 'patch'],'/seasons/{season}', [SeasonController::class, 'update']);
+    Route::delete('/seasons/{season}', [SeasonController::class, 'destroy']);
+
+
+    Route::resource('/quiz-events', QuizEventController::class)->only(['store']);
+    Route::match(['put', 'patch'],'/quiz-events/{quizEvent}', [QuizEventController::class, 'update'])->name('quit-events.update');
+    Route::delete('/quiz-events/{quizEvent}', [QuizEventController::class, 'destroy'])->name('quit-events.destroy');
+
+
+    Route::resource('/teams', TeamController::class)->only(['store']);
+    Route::match(['put', 'patch'],'/teams/{team}', [TeamController::class, 'update']);
+    Route::delete('/teams/{team}', [TeamController::class, 'destroy'])->name('teams.destroy');
+
+    Route::post('/scores', [QuizEventTeamController::class, 'store'])->name('scores.store');
+    Route::put('scores/teams/{teamId}/quiz-events/{quizEventId}', [QuizEventTeamController::class, 'update']);
+});
