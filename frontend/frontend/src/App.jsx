89e9977fd89ebd27fb3fa40d870aa8a2631eb6ef
Dusan Bjelogrlic <dusanbjelogrlic0@gmail.com>
2025-04-07 13:34:11 +0200

    Kreiran i implementiran HomePage

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Scoreboard from './components/Scoreboard';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
