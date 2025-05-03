db24295d93f873042db545e390ac1bcd9ab8905f
irinabajic <irinabajic21@gmail.com>
2025-05-03 11:33:59 +0200

    Kreirana komponenta QuestionModal  i dodata u Footer

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,42 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Scoreboard from './components/Scoreboard';
-import HomePage from './components/HomePage';
+import logo from "./logo.svg";
+import "./App.css";
+import Scoreboard from "./components/Scoreboard";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
- import { RegisterPage } from './components/RegisterPage';
- import { LoginPage } from './components/LoginPage';
- import { QuizEvents } from './components/QuizEvents';
- import Footer from './components/Footer';
-
+import { RegisterPage } from "./components/RegisterPage";
+import { LoginPage } from "./components/LoginPage";
+import { QuizEvents } from "./components/QuizEvents";
+import HomePage from "./components/HomePage";
+import Footer from "./components/Footer";
+import { useState } from "react";
+import QuestionModal from "./components/QuestionModal";
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <BrowserRouter className="App">
-       <Routes>
-        
-         <Route path="/home" element={<HomePage />} />
-         <Route
-           path="/login"
-           element={
-             <LoginPage
-              
-             />
-           }
-         />
-         <Route
-           path="/register"
-           element={<RegisterPage/>}
-         />
-        
-         <Route path="/scoreboard" element={<Scoreboard />} />
-         <Route path="/events" element={<QuizEvents />} />
-        
-       </Routes>
-       <Footer/>
-     </BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Navigate to="/home" replace={true} />}
+        ></Route>
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+        <Route path="/scoreboard" element={<Scoreboard />} />
+        <Route path="/events" element={<QuizEvents />} />
+      </Routes>
+      <Footer openModal={openModal} />
+      {isModalOpen && <QuestionModal closeModal={closeModal} />}
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
--- frontend/frontend/src/components/Footer.jsx
+++ frontend/frontend/src/components/Footer.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { FaRegCircleQuestion } from "react-icons/fa6";
 
-const Footer = () => {
+const Footer = ({ openModal }) => {
   return (
     <footer className="bg-[#1c3144] flex items-center justify-center h-screen max-h-[6vh]">
       <p className="text-sm text-white">
         &copy; {new Date().getFullYear()} FE pub
       </p>
+      <FaRegCircleQuestion
+        className="text-white ml-4 cursor-pointer"
+        onClick={openModal}
+      >
+        Open Modal
+      </FaRegCircleQuestion>
     </footer>
   );
 };
