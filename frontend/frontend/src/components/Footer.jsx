0855efe97320516f8e063855eac70c42d0e85116
Dusan Bjelogrlic <dusanbjelogrlic0@gmail.com>
2025-08-12 15:19:03 +0200

    Dodata opcija za pogadjanje zastava

--- frontend/frontend/src/components/Footer.jsx
+++ frontend/frontend/src/components/Footer.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { FaRegCircleQuestion } from "react-icons/fa6";
+import { FaRegFlag } from "react-icons/fa";
 
-const Footer = ({ openModal }) => {
+const Footer = ({ openModal, openFlagModal }) => {
   return (
-    <footer className="bg-[#1c3144] flex items-center justify-center h-screen max-h-[6vh]">
+    <footer className="bg-sky-700 flex items-center justify-center h-screen max-h-[6vh]">
       <p className="text-sm text-white">
         &copy; {new Date().getFullYear()} FE pub
       </p>
       <FaRegCircleQuestion
         className="text-white ml-4 cursor-pointer"
         onClick={openModal}
-      >
-        Open Modal
-      </FaRegCircleQuestion>
+      />
+      <FaRegFlag
+        className="text-white ml-4 cursor-pointer"
+        onClick={openFlagModal}
+      />
     </footer>
   );
 };
